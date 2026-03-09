c1cf2bfe38f37680cfe30514a56a8aba83dee253
pedroht <pedro.henrique.torres@hotmail.com>
2026-03-09 13:53:45 -0300

    chore: add getters and setters methods

--- src/main/java/com/pedroht/todolist/user/UserModel.java
+++ src/main/java/com/pedroht/todolist/user/UserModel.java
@@ -1,7 +1,31 @@
 package com.pedroht.todolist.user;
 
 public class UserModel {
-  public String name;
-  public String username;
-  public String password;
+  private String name;
+  private String username;
+  private String password;
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+  
+  public String getUsername() {
+    return this.username;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public String getName() {
+    return this.name;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  
+  public String getPassword() {
+    return this.password;
+  }
 }
