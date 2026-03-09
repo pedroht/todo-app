481101c2f66428e3886adba4b70cda76e08172d3
pedroht <pedro.henrique.torres@hotmail.com>
2026-03-09 13:49:28 -0300

    chore: create user first route

---
+++ src/main/java/com/pedroht/todolist/user/UserController.java
@@ -0,0 +1,19 @@
+package com.pedroht.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+  
+  @PostMapping("")
+  public void create(@RequestBody UserModel userModel) {
+    System.out.println(userModel.password);    
+    System.out.println(userModel.name);    
+    System.out.println(userModel.username);    
+  }  
+}
---
+++ src/main/java/com/pedroht/todolist/user/UserModel.java
@@ -0,0 +1,7 @@
+package com.pedroht.todolist.user;
+
+public class UserModel {
+  public String name;
+  public String username;
+  public String password;
+}
