fb5487b5d30632fe4d8d5647f9035ed35e04cf00
pedroht <pedro.henrique.torres@hotmail.com>
2026-03-09 17:57:51 -0300

    chore: add endpoint to create a new task

---
+++ src/main/java/com/pedroht/todolist/task/TaskController.java
@@ -0,0 +1,25 @@
+package com.pedroht.todolist.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+  @Autowired
+  ITaskRepository taskRepository;
+
+  @PostMapping("") 
+  public ResponseEntity create(@RequestBody TaskModel taskModel) {
+
+    this.taskRepository.save(taskModel);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(null);
+  }
+}
