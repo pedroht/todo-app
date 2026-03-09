6f7ca902ac46017aa89e4073d59e6669e33a5dc5
pedroht <pedro.henrique.torres@hotmail.com>
2026-03-09 17:20:17 -0300

    chore: create a repository to manage updates in the database

--- src/main/java/com/pedroht/todolist/user/UserController.java
+++ src/main/java/com/pedroht/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package com.pedroht.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+  @Autowired
+  private IUserRepository userRepository;
   
   @PostMapping("")
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.getName());    
-    System.out.println(userModel.getUsername());    
-    System.out.println(userModel.getPassword());    
+  public UserModel create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.save(userModel);
+    return user;
   }  
 }
