b122a796bb2331601473be6c96c12a77214cc659
pedroht <pedro.henrique.torres@hotmail.com>
2026-03-09 13:58:18 -0300

    chore: add lombok to the project

--- src/main/java/com/pedroht/todolist/user/UserModel.java
+++ src/main/java/com/pedroht/todolist/user/UserModel.java
@@ -1,31 +1,10 @@
 package com.pedroht.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
   private String name;
   private String username;
   private String password;
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-  
-  public String getUsername() {
-    return this.username;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-  
-  public String getName() {
-    return this.name;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-  
-  public String getPassword() {
-    return this.password;
-  }
 }
