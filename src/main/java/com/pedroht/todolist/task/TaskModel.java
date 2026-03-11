b8a377eb6454e6c88611ee49675a2fdf05dd56b7
pedroht <pedro.henrique.torres@hotmail.com>
2026-03-11 14:22:58 -0300

    chore: add custom exception + exception handler

--- src/main/java/com/pedroht/todolist/task/TaskModel.java
+++ src/main/java/com/pedroht/todolist/task/TaskModel.java
@@ -29,4 +29,12 @@ public class TaskModel {
 
   @CreationTimestamp
   private LocalDateTime createdAt;
+
+  public void setTitle(String title) throws Exception {
+    if (title.length() > 50) {
+      throw new Exception("The field title must contain only 50 caracteres!");
+    }
+
+    this.title = title;
+  }
 }
