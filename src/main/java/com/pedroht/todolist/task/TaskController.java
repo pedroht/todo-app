0d3e06fb656877c7a284b8461f2d525c41d187c4
pedroht <pedro.henrique.torres@hotmail.com>
2026-03-11 13:30:10 -0300

    chore: add endpoint to list all tasks from user

--- src/main/java/com/pedroht/todolist/task/TaskController.java
+++ src/main/java/com/pedroht/todolist/task/TaskController.java
@@ -6,12 +6,14 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
+
 
 
 @RestController
@@ -43,5 +45,13 @@ public class TaskController {
     this.taskRepository.save(taskModel);
 
     return ResponseEntity.status(HttpStatus.CREATED).body(null);
+  }
+
+  @GetMapping("") 
+  public ResponseEntity list(HttpServletRequest request) {
+    var userId = request.getAttribute("userId");
+    var tasks = this.taskRepository.findByUserId((UUID) userId);
+
+    return ResponseEntity.status(HttpStatus.OK).body(tasks);
   }
 }
